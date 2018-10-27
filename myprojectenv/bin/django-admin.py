11e458e5001adcc3bc1dfa58169ef825ad6f5738
dimasmukhlas <dimasmukhlas@gmail.com>
2018-10-27 13:54:20 +0200

    update

---
+++ myprojectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dimasmukhlas/Dropbox/projects/pythonDjango/django2trial/myprojectenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
